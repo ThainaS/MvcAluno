2758a1fdbf7119a47d3697432dba5b8f2a92ff53
ThainaS <80105495+ThainaS@users.noreply.github.com>
2021-03-05 09:22:42 -0300

    Add files via upload

---
+++ mvcaluno/Data/AlunoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using mvcaluno.Models;
+
+namespace mvcaluno.Data
+{
+    public class AlunoContext : DbContext
+    {
+        public AlunoContext (DbContextOptions<AlunoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Aluno { get; set; }
+    }
+}
